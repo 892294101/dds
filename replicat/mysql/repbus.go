5096ff00918a51cf25fac5f3af9ee411ffacd7ae
892294101 <892294101@qq.com>
2022-11-20 11:30:38 +0800

    last

--- replicat/mysql/repbus.go
+++ replicat/mysql/repbus.go
@@ -1,19 +1,1 @@
 package mysql
-
-import (
-	"github.com/892294101/dds/replicat/conn"
-	"github.com/892294101/dds/spfile"
-)
-
-type replicat struct {
-	pfile spfile.Spfile
-	conn  conn.Connector
-}
-
-func (r *replicat) InitEnv() {
-
-}
-
-func (r *replicat) DetectDataType() {
-
-}
